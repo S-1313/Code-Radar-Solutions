0ecb6b67340fe1357d688644454cbaf1d4b1f200
S-1313 <sukhpreetsinghkhalsa123@gmail.com>
2025-02-07 18:23:55 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -4,15 +4,17 @@ int main() {
     int a,b;
     scanf("%d %d",&a,&b);
     for (int n=1;n<=10;n++){
-        if (a=b*n){
+        if (a==b*n){
             
             printf("Yes");
             break;
         }
+    }
 
-        else{
-            printf("No");
-        }
+
+    if(a!=b*n){
+        printf("No");
+
     }
      
     return 0;
